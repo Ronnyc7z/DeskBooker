88ff2ceb19d7f3ef4286d7df7ccb09b6b9a253b2
Ronny Carrasquel <ronnyjcarrasquel@gmail.com>
2021-03-07 18:17:28 -0500

    initial commit

---
+++ DeskBooker.Core.Tests/Processor/DeskBookingRequestProcessor.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace DeskBooker.Core.Tests.Processor
+{
+    internal class DeskBookingRequestProcessor
+    {
+        public DeskBookingRequestProcessor()
+        {
+        }
+
+        internal DeskBookingResult BookDesk(DeskBookingRequest request)
+        {
+            if (request is null)
+                throw new ArgumentNullException("request");
+            return new DeskBookingResult
+            {
+                FirstName = request.FirstName,
+                LastName = request.LastName,
+                Email = request.Email,
+                Date = request.Date
+            };
+        }
+    }
+}
---
+++ DeskBooker.Core.Tests/Processor/DeskBookingRequestProcessorTests.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace DeskBooker.Core.Tests.Processor
+{
+    public class DeskBookingRequestProcessorTests
+    {
+        private readonly DeskBookingRequestProcessor _processor;
+        public DeskBookingRequestProcessorTests()
+        {
+            _processor = new DeskBookingRequestProcessor();
+        }
+        [Fact]
+        public void ShouldReturnDeskBookingResultWithRequestValues()
+        {
+            //Arrange
+            var request = new DeskBookingRequest
+            {
+                FirstName = "Ronny",
+                LastName = "Carrasquel",
+                Email = "ronnyjcarrasquel@gmail.com",
+                Date = new DateTime(1995, 9, 9)
+            };
+
+            //Act
+            DeskBookingResult result = _processor.BookDesk(request);
+
+            //Assert
+            Assert.NotNull(result);
+            Assert.Equal(request.FirstName, result.FirstName);
+            Assert.Equal(request.LastName, result.LastName);
+            Assert.Equal(request.Email, result.Email);
+            Assert.Equal(request.Date, result.Date);
+        }
+        [Fact]
+        public void ShouldThrowExceptionIfRequestIsNull()
+        {
+            var exception = Assert.Throws<ArgumentNullException>(() => _processor.BookDesk(null));
+
+            Assert.Equal("request", exception.ParamName);
+            
+        }
+    }
+}
