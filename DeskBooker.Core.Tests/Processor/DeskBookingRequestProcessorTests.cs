64bea715dd3d724ec079c87e632e07016271dead
Ronny Carrasquel <ronnyjcarrasquel@gmail.com>
2021-03-08 14:13:51 -0500

    refactor: create private request object

--- DeskBooker.Core.Tests/Processor/DeskBookingRequestProcessorTests.cs
+++ DeskBooker.Core.Tests/Processor/DeskBookingRequestProcessorTests.cs
@@ -6,39 +6,40 @@ namespace DeskBooker.Core.Tests.Processor
 {
     public class DeskBookingRequestProcessorTests
     {
+        private readonly DeskBookingRequest _request;
         private readonly DeskBookingRequestProcessor _processor;
         public DeskBookingRequestProcessorTests()
         {
-            _processor = new DeskBookingRequestProcessor();
-        }
-        [Fact]
-        public void ShouldReturnDeskBookingResultWithRequestValues()
-        {
-            //Arrange
-            var request = new DeskBookingRequest
+            _request = new DeskBookingRequest
             {
                 FirstName = "Ronny",
                 LastName = "Carrasquel",
                 Email = "ronnyjcarrasquel@gmail.com",
                 Date = new DateTime(1995, 9, 9)
             };
+            _processor = new DeskBookingRequestProcessor();
+        }
+        [Fact]
+        public void ShouldReturnDeskBookingResultWithRequestValues()
+        {
+            //Arrange
+            
 
             //Act
-            DeskBookingResult result = _processor.BookDesk(request);
+            DeskBookingResult result = _processor.BookDesk(_request);
             //Assert
             Assert.NotNull(result);
-            Assert.Equal(request.FirstName, result.FirstName);
-            Assert.Equal(request.LastName, result.LastName);
-            Assert.Equal(request.Email, result.Email);
-            Assert.Equal(request.Date, result.Date);
+            Assert.Equal(_request.FirstName, result.FirstName);
+            Assert.Equal(_request.LastName, result.LastName);
+            Assert.Equal(_request.Email, result.Email);
+            Assert.Equal(_request.Date, result.Date);
         }
         [Fact]
         public void ShouldThrowExceptionIfRequestIsNull()
         {
             var exception = Assert.Throws<ArgumentNullException>(() => _processor.BookDesk(null));
 
-            Assert.Equal("request", exception.ParamName);
-            
+            Assert.Equal("request", exception.ParamName);            
         }
     }
 }
