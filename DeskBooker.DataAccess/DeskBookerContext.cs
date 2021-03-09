483ad9b380e4fe7b396ba29e309d1e3c7b86d1a0
Ronny Carrasquel <ronnyjcarrasquel@gmail.com>
2021-03-09 09:58:19 -0500

    feat(dbcontext): add required classes

--- DeskBooker.DataAccess/DeskBookerContext.cs
+++ DeskBooker.DataAccess/DeskBookerContext.cs
@@ -1,7 +1,15 @@
+using DeskBooker.Core.Domain;
+using Microsoft.EntityFrameworkCore;
+
 namespace DeskBooker.DataAccess
 {
     public class DeskBookerContext : DbContext
     {
-        
+        public DeskBookerContext(DbContextOptions<DeskBookerContext> options): base(options)
+        {
+            
+        }
+        public DbSet<DeskBooking> DeskBooking { get; set; }
+        public DbSet<Desk> Desk { get; set; }        
     }
 }
---
+++ DeskBooker.DataAccess/Repositories/DeskBookingRepository.cs
@@ -0,0 +1,13 @@
+using DeskBooker.Core.DataInterface;
+using DeskBooker.Core.Domain;
+
+namespace DeskBooker.DataAccess.Repositories
+{
+    public class DeskBookingRepository : IDeskBookingRepository
+    {
+        public void Save(DeskBooking deskBooking)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
